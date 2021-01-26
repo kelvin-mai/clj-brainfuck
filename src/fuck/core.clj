9afe92bdb60237aabde58e1cd80510d29a91045c
Kelvin Mai <kelvin.mai002@gmail.com>
2021-01-25 17:57:35 -0800

    implemented whitespace

--- src/fuck/core.clj
+++ src/fuck/core.clj
@@ -18,7 +18,6 @@
 (defn value [state] (get-in state [:memory (:data-ptr state)]))
 (defn instruction [state] (get-in state [:program (:instruction-ptr state)]))
 (defn output [state]
-  (println state)
   (print (char (get-in state [:memory (:data-ptr state)]))) state)
 (defn input [state]
   state)
@@ -46,8 +45,9 @@
 (defn execute-command [state]
   (let [current-instruction (instruction state)
         run-fn (operation-map current-instruction)]
-    (when (not (fn? run-fn)) (ex-info "Command function not found." state))
-    (run-fn state)))
+    (if (not (fn? run-fn))
+      state
+      (run-fn state))))
 
 (defn step [state]
   (-> state
@@ -59,15 +59,34 @@
   (let [current-instruction (instruction state)]
     (if (nil? current-instruction)
       {:status "complete" :operations (:operations state)}
-      (recur (step state))
-      ;;
-      )))
+      (recur (step state)))))
 
 (def HELLO-WORLD "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
 
-(defn -main
-  [& args]
+(def HELLO-WORLD-2 "+++++ +++++             initialize counter (cell #0) to 10
+                   [                       use loop to set 70/100/30/10
+                   > +++++ ++              add  7 to cell #1
+                   > +++++ +++++           add 10 to cell #2
+                   > +++                   add  3 to cell #3
+                   > +                     add  1 to cell #4
+                   <<<< -                  decrement counter (cell #0)
+                   ]
+                   > ++ .                  print 'H'
+                   > + .                   print 'e'
+                   +++++ ++ .              print 'l'
+                   .                       print 'l'
+                   +++ .                   print 'o'
+                   > ++ .                  print ' '
+                   << +++++ +++++ +++++ .  print 'W'
+                   > .                     print 'o'
+                   +++ .                   print 'r'
+                   ----- - .               print 'l'
+                   ----- --- .             print 'd'
+                   > + .                   print '!'
+                   > .                     print '\n'")
+
+(defn -main []
   (-> (initialize 8)
-      (load-program HELLO-WORLD)
+      (load-program HELLO-WORLD-2)
       (run)))
 
