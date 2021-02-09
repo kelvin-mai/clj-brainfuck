41912557414cb2b5a0f19e4e67fa3ca88b4bf890
Kelvin Mai <kelvin.mai002@gmail.com>
2021-02-08 16:20:26 -0800

    clean up

--- src/fuck/core.clj
+++ src/fuck/core.clj
@@ -1,36 +1,15 @@
 (ns fuck.core)
 
-(defn initialize [memory-size]
-  {:program nil
-   :memory (vec (take memory-size (repeat 0)))
-   :data-ptr 0
-   :instruction-ptr 0
-   :operations 0
-   :while-stack (list)})
-
-(defn load-program [state program] (assoc state :program program))
-(defn instruction-pointer-inc [state] (update-in state [:instruction-ptr] inc))
-(defn instruction-pointer-reset [state] (assoc state :instruction-ptr (first (:while-stack state))))
-(defn data-pointer-inc [state] (update-in state [:data-ptr] inc))
-(defn data-pointer-dec [state] (update-in state [:data-ptr] dec))
-(defn value-inc [state] (update-in state [:memory (:data-ptr state)] inc))
-(defn value-dec [state] (update-in state [:memory (:data-ptr state)] dec))
-(defn value [state] (get-in state [:memory (:data-ptr state)]))
-(defn instruction [state] (get-in state [:program (:instruction-ptr state)]))
-(defn output [state]
-  (print (char (get-in state [:memory (:data-ptr state)]))) state)
-(defn input [state]
-  state)
-
-(defn move-while-close [state]
-  (if (= (instruction state) \]) state (recur (instruction-pointer-inc state))))
-
-(defn while-open [state]
-  (if (= (value state) 0)
-    (move-while-close state) (update-in state [:while-stack] conj (:instruction-ptr state))))
-
-(defn while-close [state]
-  (if (not= (value state) 0) (instruction-pointer-reset state) (update-in state [:while-stack] rest)))
+(declare instruction-pointer-inc
+         instruction-pointer-reset
+         data-pointer-inc
+         data-pointer-dec
+         value-inc
+         value-dec
+         output
+         input
+         while-open
+         while-close)
 
 (def operation-map
   {\> data-pointer-inc
@@ -41,6 +20,60 @@
    \, input
    \[ while-open
    \] while-close})
+
+(defn initialize [memory-size]
+  {:program nil
+   :memory (vec (take memory-size (repeat 0)))
+   :data-ptr 0
+   :instruction-ptr 0
+   :operations 0
+   :while-stack (list)})
+
+(defn load-program [state program] 
+  (assoc state :program program))
+
+(defn instruction-pointer-inc [state] 
+  (update-in state [:instruction-ptr] inc))
+
+(defn instruction-pointer-reset [state] 
+  (assoc state :instruction-ptr (first (:while-stack state))))
+
+(defn data-pointer-inc [state] 
+  (update-in state [:data-ptr] inc))
+
+(defn data-pointer-dec [state] 
+  (update-in state [:data-ptr] dec))
+
+(defn value-inc [state] 
+  (update-in state [:memory (:data-ptr state)] inc))
+
+(defn value-dec [state] 
+  (update-in state [:memory (:data-ptr state)] dec))
+
+(defn value [state] 
+  (get-in state [:memory (:data-ptr state)]))
+
+(defn instruction [state] 
+  (get-in state [:program (:instruction-ptr state)]))
+
+(defn output [state]
+  (print (char (get-in state [:memory (:data-ptr state)]))) state)
+
+(defn input [state]
+  state)
+
+(defn move-while-close [state]
+  (if (= (instruction state) \]) state (recur (instruction-pointer-inc state))))
+
+(defn while-open [state]
+  (if (= (value state) 0)
+    (move-while-close state) 
+    (update-in state [:while-stack] conj (:instruction-ptr state))))
+
+(defn while-close [state]
+  (if (not= (value state) 0)
+    (instruction-pointer-reset state) 
+    (update-in state [:while-stack] rest)))
 
 (defn execute-command [state]
   (let [current-instruction (instruction state)
